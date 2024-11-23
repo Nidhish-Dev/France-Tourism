a8a9aeba3fe4064dab2cf8116657f3c63156634b
Nidhish Rathore <codenidhish07@gmail.com>
2024-11-23 21:14:46 +0530

    added contact and about

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,9 +1,55 @@
 import React from 'react'
-
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
+import Navbar from '@/components/Navbar';
 function page() {
+  const testimonials = [
+    {
+      quote:
+        "Bonjour. Je m'appelle Nidhish, et je poursuis mes études en B.Tech en informatique avec une spécialisation en ingénierie logicielle à SRMIST Kattankulathur.Dans ce cadre, j'ai analysé la ville d'Avignon en France.",
+      name: "Nidhish Rathore",
+      designation: "Nidhish Rathore",
+      src: "/nidhish.jpg",
+    },
+    {
+      quote:
+        "Bonjour. Je m'appelle Riddhima, et je poursuis mes études en B.Tech en informatique avec une spécialisation en ingénierie logicielle à SRMIST Kattankulathur.Dans ce cadre, j'ai analysé la ville d'Le Mans en France.",
+      name: "Riddhima Singh",
+      designation: "Riddhima Singh",
+      src: "/riddhima.jpg",
+    },
+    {
+      quote:
+        "Bonjour. Je m'appelle Mourya Shree, et je poursuis mes études en B.Tech en informatique avec une spécialisation en ingénierie logicielle à SRMIST Kattankulathur.Dans ce cadre, j'ai analysé la ville d'Toulouse en France.",
+      name: "Talluri Mourya Sree",
+      designation: "Talluri Mourya Sree",
+      src: "/mourya.jpeg",
+    },
+    {
+      quote:
+        "Bonjour. Je m'appelle Soumya, et je poursuis mes études en B.Tech en informatique avec une spécialisation en ingénierie logicielle à SRMIST Kattankulathur.Dans ce cadre, j'ai analysé la ville d'Nantes en France.",
+      name: "Soumya Pandey",
+      designation: "Soumya Pandey",
+      src: "/soumya.jpg",
+    },
+    {
+      quote:
+        "Bonjour. Je m'appelle Ziya, et je poursuis mes études en B.Tech en informatique avec une spécialisation en ingénierie logicielle à SRMIST Kattankulathur.Dans ce cadre, j'ai analysé la ville d'Lyon en France.",
+      name: "Ziya Khan",
+      designation: "Ziya Khan",
+      src: "/ziya.jpeg",
+    },
+  ];
   return (
     <div>
-      
+       <div className="fixed top-0 left-0 w-full z-10 bg bg-white  bg-opacity-15 p-4 text-center fixed-content">
+   
+
+   <Navbar />
+
+ </div>
+      <div className='mt-20'>
+      <AnimatedTestimonials testimonials={testimonials} />
+      </div>
     </div>
   )
 }
--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,106 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+import Navbar from "@/components/Navbar";
 function page() {
+  const data = [
+    {
+      title: "Nidhish Rathore",
+      content: (
+        <div>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            RA2411033010097
+          </p>
+
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            nr0358@srmist.edu.in
+          </p>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            +91 870-8295706
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Riddhima Singh",
+      content: (
+        <div>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            RA2411033010107
+          </p>
+
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          rs4686@srmist.edu.in
+          </p>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          +91 708-6141199
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Talluri Mourya Sree",
+      content: (
+        <div>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            RA2411033010097
+          </p>
+
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          mt6216@srmist.edu.in
+          </p>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+        +91 799-3751014
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Soumya Pandey",
+      content: (
+        <div>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            RA2411033010097
+          </p>
+
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          sp0607@srmist.edu.in
+          </p>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          +91 886-0294972
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Ziya Khan",
+      content: (
+        <div>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+            RA2411033010095
+          </p>
+
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          zk8757@srmist.edu.in
+          </p>
+          <p className="text-white dark:text-neutral-200 text-xs md:text-lg font-normal ">
+          +91 940-7514820
+          </p>
+        </div>
+      ),
+    },
+  ];
   return (
     <div>
-      
+       <div className="fixed top-0 left-0 w-full z-10 bg bg-white  bg-opacity-15 p-4 text-center fixed-content">
+   
+
+   <Navbar />
+
+ </div>
+      <div className="w-full ">
+      <Timeline data={data} />
+    </div>
     </div>
   )
 }
